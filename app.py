4bf0d258b079b0ba7cbecff89b15343a64e5e3a1
Admin <283778512@qq.com>
2022-05-06 23:36:31 +0800

    搭建jinceM项目的开发环境

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import app
-flask_app = app.create_app("development")
+import app # 导入app模块，自动获取初始化操作
+flask_app = app.create_app("development") # 获取指定的运行环境的Flask对象
 if __name__ == '__main__':
-    flask_app.run(host="0.0.0.0",port=80,debug=True)
+    flask_app.run(host="0.0.0.0", port=80, debug=True) # 运行WEB程序
--- app/config.py
+++ app/config.py
@@ -1,18 +1,28 @@
 # coding:UTF-8
-import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config():
-    SECRET_KEY = "www.jince-tech.com-%s" % os.urandom(24)
-    MAX_CONTENT_LENGTH = 8 * 1024 *1024
-    UPLOADED_PHOTOS_DEST = os.path.join(BASE_DIR,'statci/upload')
+import os  # 模块导入
+from app.blueprints import config_blueprint
+from app.extensions import config_errorhandle, config_extensions, config_interrupt, config_filter
+from flask_wtf import CSRFProtect
+BASE_DIR = os.path.abspath(os.path.dirname(__file__)) # 设置有一个基础路径
+class Config(): # 【公共】WEB配置类
     @staticmethod
-    def init_app(app):
-        pass
-class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://jince_db_01:Jince0331@rm-bp1561n11704142t89o.mysql.rds.aliyuncs.com:3306/jince"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_ECHO = True
-config ={
-    "development":DevelopmentConfig,
+    def init_app(app): # 项目的初始化配置
+        config_blueprint(app) # 进行蓝图的相关配置
+        config_errorhandle(app) # 进行错误页的配置
+        CSRFProtect(app) # 启用CSRF保护
+        config_extensions(app) # 进行扩展包的启用配置
+        config_interrupt(app) # 配置拦截器
+        config_filter(app)
+    SECRET_KEY = "www.yootk.com-%s" % os.urandom(24)  # 定义加密配置
+    MAX_CONTENT_LENGTH = 8 * 1024 * 1024  # 上传限制
+    UPLOADED_PHOTOS_DEST = os.path.join(BASE_DIR, 'static/upload')  # 上传文件保存目录
+class DevelopmentConfig(Config): # 【某个环境】专属的配置类
+    # 数据库链接的配置，此项必须，格式为（数据库+驱动://用户名:密码@数据库主机地址:端口/数据库名称）
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://jince_db_01:Jince0331@rm-bp1561n11704142t89o.mysql.rds.aliyuncs.com:3306/yootk_flask"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True  # 对象跟踪
+    SQLALCHEMY_ECHO = True  # 对象跟踪
+config = { # 确定当前使用的系统环境配置
+    "development" : DevelopmentConfig,
+    # "test" : TestConfig,
+    # "production" : ProductionConfig,
 }
